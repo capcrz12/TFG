0701724af1e0df5e60b614c4e84b99c87aada2da
capcrz12 <carlosperezcruz01@gmail.com>
2024-04-16 11:03:50 +0200

    Intentando mapa 3D

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -24,25 +24,29 @@ export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
   
     this.map = new Map({
       container: this.mapContainer.nativeElement,
-      style: MapStyle.OUTDOOR,
+      style: `https://api.maptiler.com/maps/winter-v2/style.json?key=${config.apiKey}`,
       center: [initialState.lng, initialState.lat],
       zoom: initialState.zoom,
       pitch: 50,
       maxPitch: 85
     });
 
-    this.map.addSource("terrain", {
-      "type": "raster-dem",
-      "url": `https://api.maptiler.com/tiles/terrain-rgb-v2/tiles.json?key=${config.apiKey}`
-    });
-      
-    this.map.setTerrain({
-      source: "terrain"
-    });
+    // Añadimos una capa nueva para introducir el raster 3D
+    this.map.on('load', () => {
+      // Add new sources and layers
+      this.map.addSource("terrain", {
+          "type": "raster-dem",
+          "url": `https://api.maptiler.com/tiles/terrain-rgb-v2/tiles.json?key=${API_KEY}`
+          });
+      this.map.setTerrain({
+          source: "terrain"
+      });
+  });
+
 
     new Marker({color: "#FF0000"})
-  .setLngLat([-3.60667, 37.18817])
-  .addTo(this.map);
+      .setLngLat([-3.60667, 37.18817])
+      .addTo(this.map);
   }
   
   ngOnDestroy() {
