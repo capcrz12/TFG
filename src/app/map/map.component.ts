a3833d659227cd9f48aa47e4b868e3eb98b9d672
capcrz12 <carlosperezcruz01@gmail.com>
2024-04-26 19:01:38 +0200

    Tabla estadistica

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy, Output, EventEmitter } from '@angular/core';
 import { Map, MapStyle, Marker, config, FullscreenControl, geolocation, GeolocateControl } from '@maptiler/sdk';
 import '@maptiler/sdk/dist/maptiler-sdk.css';
 import { gpx } from "@tmcw/togeojson";
+import { Dictionary } from '../dictionary'; 
 
 @Component({
   selector: 'app-map',
@@ -15,11 +16,28 @@ export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
   gpxData: any; // Declaración de la propiedad gpxData para almacenar datos de GPX
   coordinates: any;
   bounds: any;
+  dataMap: Dictionary;
+  speed: number | undefined;
+  km: number | undefined;
+  @Output() dataMapOut = new EventEmitter<Dictionary>();
+
 
   @ViewChild('map')
   private mapContainer!: ElementRef<HTMLElement>;
 
-  constructor() {}
+  constructor() {
+    this.dataMap = {
+      'speed': -1,
+      'km': -1,
+      'des_pos': -1,
+      'des_neg': -1,
+      'maxAltitude': -1,
+      'minAltitude': -1,
+    }
+    
+    this.speed = 0;
+    this.km = 0;
+  }
 
   ngOnInit(): void {
     config.apiKey = 'xkstedU79vEq8uEaVE2A';
@@ -39,7 +57,7 @@ export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
       pitch: 50,
       maxPitch: 85,
       geolocate: false,
-      geolocateControl: false
+      geolocateControl: false,
     });
 
     this.map!.addControl(new FullscreenControl());
@@ -109,6 +127,14 @@ export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
           [this.coordinates.minLng - 0.02, this.coordinates.minLat - 0.02],
           [this.coordinates.maxLng + 0.02, this.coordinates.maxLat + 0.02]
         ];
+
+        this.dataMap['speed'] = this.calculateAverageSpeed(this.gpxData);
+        this.dataMap['maxAltitude'] = this.calculateMaxAltitude(this.gpxData);
+        this.dataMap['minAltitude'] = this.calculateMinAltitude(this.gpxData);
+        this.dataMap['km'] = parseFloat(this.calculateTotalDistance(this.gpxData).toFixed(2));
+        this.dataMap['des_neg'] = this.calculateNegativeElevationLoss(this.gpxData);
+        this.dataMap['des_pos'] = this.calculatePositiveElevationGain(this.gpxData);
+        this.dataMapOut.emit(this.dataMap);
       })
       .catch(error => {
         console.error('Error loading GPX:', error);
@@ -132,6 +158,174 @@ export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
 
     return { minLat, maxLat, minLng, maxLng };
   }
+
+  // Calcular la velocidad promedio en kilómetros por hora (km/h)
+calculateAverageSpeed(gpxData: any): number {
+  if (!gpxData || !gpxData.features || gpxData.features.length === 0) {
+    return -1;
+  }
+
+  const features = gpxData.features[0];
+  let totalDistance = 0; // en kilómetros
+  let totalTimeInSeconds = 0; // en segundos
+
+  // Recorrer cada punto del sendero para calcular la distancia y el tiempo
+  for (let i = 1; i < features.geometry.coordinates.length; i++) {
+    const prevCoord = features.geometry.coordinates[i-1];
+    const currCoord = features.geometry.coordinates[i];
+
+    const distance = this.calculateDistance(prevCoord[1], prevCoord[0], currCoord[1], currCoord[0]); // en kilómetros
+    const prev = new Date(features.properties.coordinateProperties.times[i-1]).valueOf();
+    const curr = new Date(features.properties.coordinateProperties.times[i]).valueOf();
+    const timeDiffInSeconds = (curr - prev) / 1000;
+
+    totalDistance += distance;
+    totalTimeInSeconds += timeDiffInSeconds;
+  }
+
+  // Calcular velocidad promedio en km/h
+  if (totalTimeInSeconds > 0) {
+    const averageSpeed = (totalDistance / (totalTimeInSeconds / 3600)).toFixed(2); // convertir a km/h
+    return parseFloat(averageSpeed);
+  }
+
+  return -1;
+}
+
+// Calcular distancia entre dos coordenadas geográficas utilizando la fórmula haversine
+calculateDistance(lat1: number, lon1: number, lat2: number, lon2: number): number {
+  const R = 6371; // radio de la Tierra en kilómetros
+  const dLat = this.degreesToRadians(lat2 - lat1);
+  const dLon = this.degreesToRadians(lon2 - lon1);
+
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(this.degreesToRadians(lat1)) * Math.cos(this.degreesToRadians(lat2)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = R * c; // distancia en kilómetros
+  return distance;
+}
+
+calculateMaxAltitude(gpxData: any): number {
+  if (!gpxData || !gpxData.features || gpxData.features.length === 0) {
+    return -1;
+  }
+
+  let maxAltitude = -Infinity;
+  const features = gpxData.features[0];
+
+  // Recorrer cada punto del sendero para encontrar la altitud máxima
+  for (let i = 1; i < features.geometry.coordinates.length; i++) {
+
+    let altitude = features.geometry.coordinates[i][2]; // La altitud es el tercer elemento del array de coordenadas
+    if (altitude > maxAltitude) {
+      maxAltitude = altitude;
+    }
+  }
+
+  // Si maxAltitude sigue siendo igual a -Infinity, significa que no se encontraron altitudes válidas
+  // en el archivo GPX
+  if (maxAltitude === -Infinity) {
+    return -1;
+  }
+
+  return parseFloat(maxAltitude.toFixed(0));
+}
+
+calculateMinAltitude(gpxData: any): number {
+  if (!gpxData || !gpxData.features || gpxData.features.length === 0) {
+    return -1;
+  }
+
+  let minAltitude = Infinity;
+  const features = gpxData.features[0];
+
+  // Recorrer cada punto del sendero para encontrar la altitud máxima
+  for (let i = 1; i < features.geometry.coordinates.length; i++) {
+
+    let altitude = features.geometry.coordinates[i][2]; // La altitud es el tercer elemento del array de coordenadas
+    if (altitude < minAltitude) {
+      minAltitude = altitude;
+    }
+  }
+
+  // Si maxAltitude sigue siendo igual a -Infinity, significa que no se encontraron altitudes válidas
+  // en el archivo GPX
+  if (minAltitude === Infinity) {
+    return -1;
+  }
+
+  return parseFloat(minAltitude.toFixed(0));
+}
+
+calculatePositiveElevationGain(gpxData: any): number {
+  if (!gpxData || !gpxData.features || gpxData.features.length === 0) {
+    return -1;
+  }
+
+  let elevationGain = 0;
+
+  // Recorrer cada punto del sendero para calcular el desnivel positivo
+  for (let i = 1; i < gpxData.features[0].geometry.coordinates.length; i++) {
+    const prevCoord = gpxData.features[0].geometry.coordinates[i - 1];
+    const currCoord = gpxData.features[0].geometry.coordinates[i];
+    const elevationChange = currCoord[2] - prevCoord[2]; // Cambio de altitud entre dos puntos
+    if (elevationChange > 0) {
+      elevationGain += elevationChange;
+    }
+  }
+
+  return parseFloat(elevationGain.toFixed(0));
+}
+
+calculateNegativeElevationLoss(gpxData: any): number {
+  if (!gpxData || !gpxData.features || gpxData.features.length === 0) {
+    return -1;
+  }
+
+  let elevationLoss = 0;
+
+  // Recorrer cada punto del sendero para calcular el desnivel negativo
+  for (let i = 1; i < gpxData.features[0].geometry.coordinates.length; i++) {
+    const prevCoord = gpxData.features[0].geometry.coordinates[i - 1];
+    const currCoord = gpxData.features[0].geometry.coordinates[i];
+    const elevationChange = currCoord[2] - prevCoord[2]; // Cambio de altitud entre dos puntos
+    if (elevationChange < 0) {
+      elevationLoss += Math.abs(elevationChange); // Convertir a valor absoluto
+    }
+  }
+
+  return parseFloat(elevationLoss.toFixed(0));
+}
+
+
+// Convertir grados a radianes
+degreesToRadians(degrees: number): number {
+  return degrees * (Math.PI / 180);
+}
+
+calculateTotalDistance(gpxData: any): number {
+  if (!gpxData || !gpxData.features || gpxData.features.length === 0) {
+    return 0;
+  }
+
+  let totalDistance = 0; // en kilómetros
+
+  // Recorrer cada punto del sendero para calcular la distancia total
+  for (let i = 1; i < gpxData.features[0].geometry.coordinates.length; i++) {
+    const prevCoord = gpxData.features[0].geometry.coordinates[i-1];
+    const currCoord = gpxData.features[0].geometry.coordinates[i];
+    const distance = this.calculateDistance(prevCoord[1], prevCoord[0], currCoord[1], currCoord[0]); // en kilómetros
+    totalDistance += distance;
+    totalDistance = parseFloat(totalDistance.toFixed(2));
+  }
+
+  return totalDistance;
+}
+
+
   
   ngOnDestroy() {
     this.map?.remove();
--- src/app/ruta/ruta.component.ts
+++ src/app/ruta/ruta.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { MapComponent } from '../map/map.component';
 import { RouterOutlet } from '@angular/router';
+import { Dictionary } from '../dictionary';
 
 @Component({
   selector: 'app-ruta',
@@ -11,4 +12,47 @@ import { RouterOutlet } from '@angular/router';
 })
 export class RutaComponent {
   @Input() name = '';
+
+  dataMap: Dictionary;
+
+  observer: IntersectionObserver | undefined;
+
+
+  constructor() {
+    this.dataMap = {}
+  }
+
+  setupIntersectionObserver(): void {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5 // Define el umbral de visibilidad al 50%
+    };
+
+    // Obtener todos los elementos <p> dentro de #tabla-section
+    const paragraphs = document.querySelectorAll('#tabla-seccion p');
+  
+    // Configurar un observador de intersección para cada <p>
+    paragraphs.forEach(p => {
+      this.observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            // Cuando el <p> es visible, aplicar una animación o clase
+            p.classList.add('active');
+          } else {
+            // Cuando el <p> no es visible, revertir la animación o clase
+            p.classList.remove('active');
+          }
+        });
+      }, options);
+
+      // Observar el elemento <p> actual
+      this.observer!.observe(p);
+    });
+  }
+
+  receiveDataMap(dic:Dictionary) {
+    this.dataMap = dic;
+    this.setupIntersectionObserver();
+  }
 }
