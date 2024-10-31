adf4def1d70f9dc8d32f3ca5dc881c43d58acd7e
capcrz12 <carlosperezcruz01@gmail.com>
2024-10-31 13:10:24 +0100

    env

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    APIUrl: 'tfg-api-production-3337.up.railway.app',
+    URL: 'http://localhost:4200/',
+    mapKey: 'xkstedU79vEq8uEaVE2A'
+  };
