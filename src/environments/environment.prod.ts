0930aeeb06589269a88a485b8c09855b51b7eb2c
Carlos Pérez Cruz <92938495+capcrz12@users.noreply.github.com>
2024-10-31 12:50:43 +0100

    Delete src/environments directory

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    APIUrl: process.env['API_URL'] || 'tfg-api-production-3337.up.railway.app',
-    URL: 'http://localhost:4200/',
-    mapKey: 'xkstedU79vEq8uEaVE2A'
-  };
