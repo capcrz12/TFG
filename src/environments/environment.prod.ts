9518b59ce20f56ba44f14f87f107d625a2af1a72
capcrz12 <carlosperezcruz01@gmail.com>
2024-11-09 12:25:16 +0100

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    APIUrl: 'tfg-api-production-3337.up.railway.app',
+    APIUrl: 'https://api-sigma-liard.vercel.app/',
     URL: 'http://localhost:4200/',
     mapKey: 'xkstedU79vEq8uEaVE2A'
   };
