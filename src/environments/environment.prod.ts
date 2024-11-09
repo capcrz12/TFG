fac76cd1beaa058d3969b5b1d02f4f3dd56f5f19
capcrz12 <carlosperezcruz01@gmail.com>
2024-11-09 20:42:39 +0100

    env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    APIUrl: 'https://api-fb63zik52-capcrz12s-projects.vercel.app/',
+    APIUrl: 'https://api-capcrz12-capcrz12s-projects.vercel.app/',
     URL: 'http://localhost:4200/',
     mapKey: 'xkstedU79vEq8uEaVE2A'
   };
