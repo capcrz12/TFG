1a39274f020be9f0e102ae11c40bccf287599256
Carlos Pérez Cruz <92938495+capcrz12@users.noreply.github.com>
2024-11-10 22:49:29 +0100

    Delete src/environments directory

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    APIUrl: 'http://localhost:8000/',
-    URL: 'http://localhost:4200/',
-    mapKey: 'xkstedU79vEq8uEaVE2A'
-};
--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    APIUrl: 'https://tfg-api-7wh8.onrender.com/',
-    URL: 'http://localhost:4200/',
-    mapKey: 'xkstedU79vEq8uEaVE2A'
-  };
