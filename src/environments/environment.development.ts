6880c7a60f39769664fd4e61dee4fd26cd519291
capcrz12 <carlosperezcruz01@gmail.com>
2024-11-10 23:44:58 +0100

    env mynode config

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+    APIUrl: 'http://localhost:8000/',
+    URL: 'http://localhost:4200/',
+    mapKey: 'xkstedU79vEq8uEaVE2A'
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,10 @@
+export const environment = {};
+
+/*
 export const environment = {
     production: false,
     APIUrl: 'http://localhost:8000/',  
     URL: 'http://localhost:4200/',
     mapKey: 'xkstedU79vEq8uEaVE2A'
 };
+*/
