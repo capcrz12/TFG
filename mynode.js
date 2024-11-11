8e6fd6d5061c2f34b14b4aa8a355cc98697d8ef8
capcrz12 <carlosperezcruz01@gmail.com>
2024-11-11 11:37:06 +0100

    env config

--- mynode.js
+++ mynode.js
@@ -4,27 +4,37 @@ const successColor = '\x1b[32m%s\x1b[0m';
 const checkSign = '\u{2705}';
 const dotenv = require('dotenv').config({path: 'src/.env'}); ;
 
-const envFile = `export const environment = {
+const prodFile = `export const environment = {
     APIUrl: '${process.env.APIUrl}',
     mapKey: '${process.env.mapKey}'
 };
 `;
 
-const targetDir = path.join(__dirname, './src/environments');
-const targetPath = path.join(targetDir, 'environment.prod.ts');
+const envFile = `export const environment = {};`;
 
-console.log(successColor, `${checkSign} ${targetPath}`);
+const targetDir = path.join(__dirname, './src/environments');
+const envPath = path.join(targetDir, 'environment.ts');
+const prodPath = path.join(targetDir, 'environment.prod.ts');
 
 // Crear la carpeta 'environments' si no existe
 if (!fs.existsSync(targetDir)) {
     fs.mkdirSync(targetDir, { recursive: true });
 }
 
-fs.writeFile(targetPath, envFile, (err) => {
+fs.writeFile(envPath, envFile, (err) => {
     if (err) {
         console.error(err);
         throw err;
     } else {
-        console.log(successColor, `${checkSign} Successfully generated environment.development.ts`);
+        console.log(successColor, `${checkSign} Successfully generated environment.ts`);
+    }
+});
+
+fs.writeFile(prodPath, prodFile, (err) => {
+    if (err) {
+        console.error(err);
+        throw err;
+    } else {
+        console.log(successColor, `${checkSign} Successfully generated environment.prod.ts`);
     }
 });
